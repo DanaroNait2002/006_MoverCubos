57817a87eaa527159e4b1182599868fc6d5bfd8e
Daniela Rodríguez <danaronait2002@gmail.com>
2022-11-17 10:42:40 +0000

    Pausa Recreo

--- Assets/Scripts/IntentoDeScript.cs
+++ Assets/Scripts/IntentoDeScript.cs
@@ -18,66 +18,129 @@ public class IntentoDeScript : MonoBehaviour
 
      */
 
+    public enum StateSelector{
+        Waiting,
+        Selection,
+        Moving,
+        Realease,
+        Scale,
+        Rotation,
+        //... the states that we need
+    }
+
+    //
+    [SerializeField]
+    StateSelector currentState = StateSelector.Waiting;
+
     //The selected object variable
     public GameObject selectedItem;
 
     private void Update()
     {
+        //Instead of if we can use Switch
+        switch (currentState)
+        {
+            case StateSelector.Waiting:
+                currentState = StateSelector.Selection;
+                break;
+
+            case StateSelector.Moving:
+                MoveItem();
+                break;
+
+            case StateSelector.Selection:
+                SelectItem();
+                break;
+
+            case StateSelector.Realease:
+                RealeaseItem();
+                break;
+        }
+        /*
         //If left click is Pressed
         if (Input.GetMouseButtonDown(0))
         {
             //If tthere´s an object selected
             if (selectedItem == null)
             {
-                //Send Ray from cam to the mouse's position
-                Vector3 pos = Input.mousePosition;
-                Ray rayo = Camera.main.ScreenPointToRay(pos);
-                RaycastHit hitInfo;
-
-                if (Physics.Raycast(rayo, out hitInfo))
-                {
-                    //Comparing the tag of the hitted object with an specific one
-                    if (hitInfo.collider.tag.Equals("Clickable"))
-                    {
-                        //The hitted object is the selected object
-                        selectedItem = hitInfo.collider.gameObject;
-
-                        //Hide Cursor
-                        Cursor.visible = false;
-                    }
-                }
+                SelectItem();
             }
             //In case that the hitted object is not null
             else
-            {              
-                //Realease the object
-                selectedItem = null;
-
-                //Show Cursor
-                Cursor.visible = true;
+            {
+                RealeaseItem();
             }
         }
 
         //If there´s no object selected
         if (selectedItem != null)
         {
+            MoveItem();
+        }*/
+    }
+        
+
+    void SelectItem()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
             //Send Ray from cam to the mouse's position
             Vector3 pos = Input.mousePosition;
             Ray rayo = Camera.main.ScreenPointToRay(pos);
             RaycastHit hitInfo;
 
-            //Hide the object to avoid hitting it with the new Ray
-            selectedItem.SetActive(false);
-
             if (Physics.Raycast(rayo, out hitInfo))
             {
-                //Moving the object
-                selectedItem.transform.position = hitInfo.point + ((Vector3.up * selectedItem.transform.localScale.y) / 2);
-            }
+                //Comparing the tag of the hitted object with an specific one
+                if (hitInfo.collider.tag.Equals("Clickable"))
+                {
 
-            //Show the object
-            selectedItem.SetActive(true);
+                    //The hitted object is the selected object
+                    selectedItem = hitInfo.collider.gameObject;
+
+                    currentState = StateSelector.Moving;
+
+                    //Hide Cursor
+                    Cursor.visible = false;
+                }
+            }
+        }
+        
+    }
+
+    void MoveItem()
+    {
+        //Send Ray from cam to the mouse's position
+        Vector3 pos = Input.mousePosition;
+        Ray rayo = Camera.main.ScreenPointToRay(pos);
+        RaycastHit hitInfo;
+
+        //Hide the object to avoid hitting it with the new Ray
+        selectedItem.SetActive(false);
+
+        if (Physics.Raycast(rayo, out hitInfo))
+        {
+            //Moving the object
+            selectedItem.transform.position = hitInfo.point + ((Vector3.up * selectedItem.transform.localScale.y) / 2);
+        }
+
+        //Show the object
+        selectedItem.SetActive(true);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            currentState = StateSelector.Realease;
         }
     }
 
+    void RealeaseItem()
+    {
+        //Realease the object
+        selectedItem = null;
+
+        currentState = StateSelector.Waiting;
+
+        //Show Cursor
+        Cursor.visible = true;
+    }
 }
